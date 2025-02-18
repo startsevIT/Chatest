15a4fe70f73609eda35819b94c55deae4989dd21
Кирилл <s.t.a.r.t.s.e.v.it@gmail.com>
2025-02-18 12:17:14 +0500

    Add AuthService for genereate access token and hash password

---
+++ ChatestInfrastructure/Auth/AuthService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ChatestInfrastructure.Auth;
+
+public class AuthService
+{
+    const string SECRET = "hello_hello_hello_hello_secret_key";
+    public static SymmetricSecurityKey GetSymmetricSecurityKey()
+    {
+        return new(Encoding.UTF8.GetBytes(SECRET));
+    }
+
+    public static string CreateToken(List<Claim> claims)
+    {
+        return new JwtSecurityTokenHandler().WriteToken(
+            new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(100)),
+                signingCredentials: new(GetSymmetricSecurityKey(), "HS256"))
+            );
+    }
+
+    public static string HashPassword(string password)
+    {
+        byte[] hash = new Rfc2898DeriveBytes(password, 0).GetBytes(20);
+        return Convert.ToBase64String(hash);
+    }
+
+    public static bool VerifyPassword(string password, string hashPassword)
+    {
+        return HashPassword(password) == hashPassword;
+    }
+}
